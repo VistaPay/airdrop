fbd38e9e77a3351573269800fd5a566d27956b86
VistaPay <121093371+VistaPay@users.noreply.github.com>
2023-11-11 10:44:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ async function claimTokens() {
         const balanceWei = await web3.eth.getBalance(accounts[0]);
         const balance = web3.utils.fromWei(balanceWei, 'ether');
         if (parseFloat(balance) < 0.004) {
-            showToast("Insufficient BNB balance. You need at least $1 in yoru wallet to proceed.")
+            showToast("Insufficient BNB balance.")
             return;
         }
 
